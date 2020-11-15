0367820eb5f69b109436daec8222eb670a2f4e61
Soft_Coder <70235748+softcoder24@users.noreply.github.com>
2020-11-15 11:15:21 +0330

    minor change in credentials variable name
    1. minor changes in credentials variable name
    2. default caption set to None

--- instagram.py
+++ instagram.py
@@ -4,9 +4,9 @@ import requests
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Credentials >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-username = "YOUR USERNAME"
-password = "PASSWORD"
-caption = "This is awesome"
+insta_user = "YOUR USERNAME"
+insta_password = "PASSWORD"
+
 
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Login into instagram account >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -26,7 +26,8 @@ def login_instagram(username, password):
     }
 
     login_header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)"
+                      " Chrome/77.0.3865.120 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest",
         "Referer": "https://www.instagram.com/accounts/login/",
         "x-csrftoken": csrf
@@ -47,9 +48,9 @@ def login_instagram(username, password):
         print("login failed ", login_response.text)
 
 
-def share_photo(photo, caption):
+def share_photo(photo, caption=None):
     micro_time = int(datetime.now().timestamp())
-    csrf_token, session_id = login_instagram(username, password)
+    csrf_token, session_id = login_instagram(insta_user, insta_password)
 
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< upload a photo into instagram server >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -58,9 +59,11 @@ def share_photo(photo, caption):
         "content-length": "1",
         "X-Entity-Name": f"fb_uploader_{micro_time}",
         "Offset": "0",
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)"
+                      " Chrome/77.0.3865.120 Safari/537.36",
         "x-entity-length": "1",
-        "X-Instagram-Rupload-Params": f'{{"media_type": 1, "upload_id": {micro_time}, "upload_media_height": 1080, "upload_media_width": 1080}}',
+        "X-Instagram-Rupload-Params": f'{{"media_type": 1, "upload_id": {micro_time}, "upload_media_height": 1080,'
+                                      f' "upload_media_width": 1080}}',
         "x-csrftoken": csrf_token,
         "x-ig-app-id": "1217981644879628",
         "cookie": "sessionid=" + session_id + "; csrftoken=" + csrf_token + ";"
@@ -78,14 +81,16 @@ def share_photo(photo, caption):
 
         url = "https://www.instagram.com/create/configure/"
 
-        payload = 'upload_id=' + upload_id + '&caption=' + caption + '&usertags=&custom_accessibility_caption=&retry_timeout='
+        payload = 'upload_id=' + upload_id + '&caption=' + caption +\
+                  '&usertags=&custom_accessibility_caption=&retry_timeout='
         headers = {
             'authority': 'www.instagram.com',
             'x-ig-www-claim': 'hmac.AR2-43UfYbG2ZZLxh-BQ8N0rqGa-hESkcmxat2RqMAXejXE3',
             'x-instagram-ajax': 'adb961e446b7-hot',
             'content-type': 'application/x-www-form-urlencoded',
             'accept': '*/*',
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/85.0.4183.121 Safari/537.36',
             'x-requested-with': 'XMLHttpRequest',
             'x-csrftoken': csrf_token,
             'x-ig-app-id': '1217981644879628',
@@ -108,4 +113,4 @@ def share_photo(photo, caption):
         print(json_data)
 
 
-share_photo(photo="toronto.jpg", caption)
+share_photo(photo="toronto.jpg", caption="This is awesome")
