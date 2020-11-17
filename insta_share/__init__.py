5b3a58123facf7515e07f882faec0d507a1cbf3a
softcoder24 <softcoder93@gmail.com>
2020-11-17 19:26:33 +0330

    Package published into pypi!

--- insta_share/__init__.py
+++ insta_share/__init__.py
@@ -1,1 +1,3 @@
 from .instagram import Instagram
+
+__version__ = "0.0.1"
--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
+import re
+
 import setuptools
+
+with open('insta_share/__init__.py') as f:
+    version = re.search(r'([0-9]+(\.dev|\.|)){3}', f.read()).group(0)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="insta_share",
-    version="0.0.1",
+    version=version,
     author="soft_coder",
     description="Python package for sharing on Instagram",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/softcoder24/insta_share",
     packages=setuptools.find_packages(),
+    license='apache-2.0',
+    zip_safe=False,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3'
     ],
     python_requires='>=3.6',
 )
